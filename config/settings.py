b9839cfdb470de0bc164ff0688bb926ca9e661a8
iamcerebrocerberus <sethamekmen@gmail.com>
2025-07-16 01:06:33 +0000

    fix CSRF ORIGIN

--- config/settings.py
+++ config/settings.py
@@ -129,3 +129,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://ieee-demo.fly.dev',
+]
